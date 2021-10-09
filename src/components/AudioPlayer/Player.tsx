6cfdb7c68e8b3b1639dc534b5696f448e7986f77
Bannersx <manuelmarinlopez07@gmail.com>
2021-10-09 01:48:18 -0600

    Player.tsx Added
    Started working on the song player.
    Implemented a hook to help facilitate things.

---
+++ src/components/AudioPlayer/Player.tsx
@@ -0,0 +1,79 @@
+import React, { FunctionComponent, useRef } from "react";
+import Audiohook from './PlayerHook';
+import {MdPlayArrow, MdPause, MdVolumeMute, MdVolumeUp} from 'react-icons/md';
+import Icon from "@chakra-ui/icon";
+import { Select, Flex, IconButton } from "@chakra-ui/react";
+interface AudioPlayerProps{}
+
+const AudioPlayer : FunctionComponent<AudioPlayerProps> = () => {
+
+    const videoElement = useRef(null);
+
+    const {                                     // Setting up the hook
+        playing,
+        progress,
+        speed,
+        muted,
+        togglePlay,
+        handleOnTimeUpdate,
+        handleVideoProgress,
+        handleVideoSpeed,
+        toggleMute
+    }   = Audiohook(videoElement);
+    return(
+        <>
+            <div className="container">
+                <div className="video-wrapper">
+                    <video
+                    /* src={video} */
+                    ref={videoElement}
+                    onTimeUpdate={handleOnTimeUpdate}
+                    />
+                    <div className="controls">
+                    <Flex>
+                        <div className="actions">
+                            <IconButton aria-label='playButton' variant='ghost' onClick={togglePlay}>
+                            {!playing ? (
+                                <Icon as={MdPlayArrow}></Icon>
+                            ) : (
+                                <Icon as={MdPause}></Icon>
+                            )}
+                            </IconButton>
+                        </div>
+                        <input
+                            type="range"
+                            min="0"
+                            max="100"
+                            value={progress}
+                            onChange={(e) => handleVideoProgress(e)}
+                        />
+                        <Select
+                            className="velocity"
+                            variant='ghost'
+                            value={speed}
+                            onChange={(e) => handleVideoSpeed(e)}
+                            width='70px'
+                            height='30px'
+                            mt='5px'
+                        >
+                            <option value="0.50">0.50x</option>
+                            <option value="1">1x</option>
+                            <option value="1.25">1.25x</option>
+                            <option value="2">2x</option>
+                        </Select>
+                        <button className="mute-btn" onClick={toggleMute}>
+                            {!muted ? (
+                            <Icon as={MdVolumeUp}></Icon>
+                            ) : (
+                            <Icon as={MdVolumeMute}></Icon>
+                            )}
+                        </button>
+                    </Flex>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default AudioPlayer;
