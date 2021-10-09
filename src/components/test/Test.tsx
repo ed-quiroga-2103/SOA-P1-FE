6564d55f137bbe3099c4198def0b502cc0c50bf0
Bannersx <manuelmarinlopez07@gmail.com>
2021-10-08 21:15:09 -0600

    Testing the new components
    General tests

--- src/components/test/Test.tsx
+++ src/components/test/Test.tsx
@@ -1,9 +1,14 @@
+import { Container } from '@chakra-ui/layout';
 import { FC } from 'react';
+import About from '../Information/About';
+import Contact from '../Information/Contact';
 
 interface TestProps {}
 
 const Test: FC<TestProps> = () => {
-    return <></>;
+    return <>
+       <About></About>
+    </>;
 };
 
 export default Test;
