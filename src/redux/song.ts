c0743dcd5cef307b2c3d75b89622708a614e2316
Bannersx <manuelmarinlopez07@gmail.com>
2021-10-10 12:08:14 -0600

    Update song.ts
    Updated reducers

--- src/redux/song.ts
+++ src/redux/song.ts
@@ -11,15 +11,24 @@ export const slice = createSlice({
     },
     reducers: {
         setName: (state, values) => {
+            localStorage.setItem('name', values.payload);
+
             state.name = values.payload;
+
         },
         setArtist: (state, values) => {
+            localStorage.setItem('artist', values.payload);
+
             state.artist = values.payload;
         },
         setAlbum: (state, values) => {
+            localStorage.setItem('album', values.payload);
+
             state.album = values.payload;
         },
         setLyrics: (state, values) => {
+            localStorage.setItem('lyrics', values.payload);
+
             state.lyrics = values.payload;
         },
     },
@@ -28,9 +37,28 @@ export const slice = createSlice({
 // Action creators are generated for each case reducer function
 export const { setName, setArtist, setAlbum, setLyrics } = slice.actions;
 
-export const getName = (state: RootState) => state.song.name;
-export const getArtist = (state: RootState) => state.song.artist;
-export const getAlbum = (state: RootState) => state.song.album;
-export const getLyrics = (state: RootState) => state.song.lyrics;
+export const getName = (state: RootState) => {
+    const name = localStorage.getItem('name');
+    if (name) return name;
+    return state.song.name;
+};
+
+export const getArtist = (state: RootState) => {
+    const artist = localStorage.getItem('artist');
+    if (artist) return artist;
+    return state.song.artist;
+};
+
+export const getAlbum = (state: RootState) => {
+    const album = localStorage.getItem('album');
+    if (album) return album;
+    return state.song.album;
+};
+
+export const getLyrics = (state: RootState) => {
+    const lyrics = localStorage.getItem('lyrics');
+    if (lyrics) return lyrics;
+    return state.song.lyrics;
+};
 
 export default slice.reducer;
